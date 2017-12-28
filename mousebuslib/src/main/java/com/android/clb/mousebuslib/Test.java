1e2aa39f8f9c2babd7c633162cd8875176427657
mousebean <mousebean.163.com>
2017-12-29 01:06:35 +0800

    Initial commit

---
+++ mousebuslib/src/main/java/com/android/clb/mousebuslib/Test.java
@@ -0,0 +1,11 @@
+package com.android.clb.mousebuslib;
+
+/**
+ * Created by linbin on 2017/12/29.
+ */
+
+public class Test {
+    public double add(double d1,double d2){
+        return d1+d2;
+    }
+}
